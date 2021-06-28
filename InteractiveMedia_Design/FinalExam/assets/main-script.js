0aacaa95a495f9d697a127a0e532285f56414740
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2021-06-28 23:12:29 +0900

    Add final exam folder

---
+++ InteractiveMedia_Design/FinalExam/assets/main-script.js
@@ -0,0 +1,34 @@
+function createApp() {
+    new Vue({
+        el: "#timetableapp",
+        data: {
+            currentTabIndex: 0,
+            tabs: [{
+                    id: "senju",
+                    name: "東京千住キャンパス",
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodilaboriosam aut earum, quia modi deleniti officia perspiciatis aliquamomnis.A alias, velit aut at dolorum animi molestiae aliquid ipsamaxime ? ",
+                },
+                {
+                    id: "hatoyama",
+                    name: "鳩山キャンパス",
+                    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integerenim diam, laoreet a nisi id, bibendum congue orci. Nullam id auctorelit, non sollicitudin est. Quisque rutrum, nulla a aliquamfacilisis, tellus ligula pellentesque Lorem ipsum dolor sit amet,consectetur adipiscing elit. Integer enim diam, laoreet a nisi id,bibendum congue orci. Nullam id auctor elit, non sollicitudin est. Quisque rutrum, nulla a aliquam facilisis, tellus ligula pellentesque Lorem ipsum dolor sit amet, consectetur adipiscingelit. Integer enim diam, laoreet a nisi id, bibendum congue orci. Nullam id auctor elit, non sollicitudin est. Quisque rutrum, nulla a aliquam facilisis, tellus ligula pellentesque"
+                },
+            ]
+        },
+        computed: {
+            currentTabContent() {
+                return this.tabs[this.currentTabIndex].content
+            }
+        },
+        methods: {
+            onClick(index) {
+                this.currentTabIndex = index
+            }
+        },
+    })
+}
+
+function initialize() {
+    createApp()
+}
+document.addEventListener("DOMContentLoaded", initialize.bind(this))
