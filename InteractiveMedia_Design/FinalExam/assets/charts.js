b75e2b7ac3479d78d8ddc1d90601bcffb48a3ee3
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2021-06-30 16:25:07 +0900

    Add Charts

---
+++ InteractiveMedia_Design/FinalExam/assets/charts.js
@@ -0,0 +1,17 @@
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+        labels: ["すごく良い", "良い", "あまり良くない", "良くない"],
+        datasets: [{
+            backgroundColor: ["#BB5179", "#FAFF67", "#58A27C", "#3C00FF"],
+            data: [66.7, 32.9, 0.4, 0],
+        }, ],
+    },
+    options: {
+        title: {
+            display: true,
+            text: "来場者アンケート",
+        },
+    },
+});
